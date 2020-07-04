4450962e587baaa3b279154b78afa8a49ea1acff
denis <dineresc@gmail.com>
2020-07-04 01:20:32 -0300

    criando o projeto

---
+++ src/main/java/com/gladguys/polisapi/PolisApiApplication.java
@@ -0,0 +1,13 @@
+package com.gladguys.polisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolisApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gladguys/polisapi/PolisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gladguys.polisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
